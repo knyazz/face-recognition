ed0b9df70e55dde54040c35abb9b8b0b69793d5e
Evgenii Smirnov <knyazz@gmail.com>
2021-11-04 14:38:32 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,33 @@
-[metadata]
-name = python-face-recognition-wrapper
-version = 0.1.0
-author = Smirnov.EV
-author_email = knyazz@gmail.com
-description = wrapper of face recognition sdk to compare faces
-long_description = file: README.md
-long_description_content_type = text/markdown
-url = https://github.com/knyazz/face-recognition
-classifiers =
-    Programming Language :: Python
-    License :: OSI Approved :: MIT License
-    Operating System :: OS Independent
-    Topic :: Software Development :: Libraries :: Python Modules
-    Intended Audience :: Developers
-    Development Status :: 4 - Beta
+import setuptools
 
-[options]
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-install_requires =
-    numpy==1.21.3
-    Pillow==8.4.0
-    face_recognition_models==0.3.0
-    dlib==19.22.1
-    click==8.0.3
-package_dir =
-    = src
-packages = find:
-python_requires = >=3.6
-
-[options.packages.find]
-where = src
+setuptools.setup(
+    name='python-face-recognition-wrapper',
+    version='0.1.0',
+    author='Smirnov.EV',
+    author_email='knyazz@gmail.com',
+    description='wrapper of face recognition sdk to compare faces',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/knyazz/face-recognition',
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta'
+    ],
+    install_requires=[
+        'numpy==1.21.3',
+        'Pillow==8.4.0',
+        'face_recognition_models==0.3.0',
+        'dlib==19.22.1',
+        'click==8.0.3',
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6"
+)
